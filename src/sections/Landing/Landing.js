d944003bbf60a6a5ed5d1d505f24742c3148fe18
Graham Gumbert <gjgumbert@gmail.com>
2022-09-05 01:25:26 -0400

    added code of conduct to landing page

--- src/sections/Landing/Landing.js
+++ src/sections/Landing/Landing.js
@@ -13,26 +13,50 @@ const Landing = () => {
           <div className="descriptor">Code for Good</div>
           <div className="line"></div>
           <div className="date">October 22-23, 2022</div>
+          <div className="button-conduct">
+            <a
+              className="join"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://hackduke.typeform.com/to/XYySGCPT"
+            >
+              Apply Now!
+            </a>
+            <div className="conduct">
+              © HackDuke 2022 —{" "}
+              <a
+                className="conduct-link"
+                href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+                target="_blank"
+              >
+                Code of Conduct
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img className="island" src={island} />
+        <div className="button-conduct-mobile button-conduct">
           <a
-            className="join"
+            className="join-mobile join"
             target="_blank"
             rel="noopener noreferrer"
             href="https://hackduke.typeform.com/to/XYySGCPT"
           >
             Apply Now!
           </a>
+          <div className="conduct">
+            © HackDuke 2022 —{" "}
+            <a
+              className="conduct-link"
+              href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+              target="_blank"
+            >
+              Code of Conduct
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="right">
-        <img className="island" src={island} />
-        <a
-          className="join-mobile join"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://hackduke.typeform.com/to/XYySGCPT"
-        >
-          Apply Now!
-        </a>
       </div>
     </div>
   );
