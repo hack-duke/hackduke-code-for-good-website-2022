0645d4664a1e90d47ce4d302aff0afb77d9c464f
Evan Glas <eglas27@gmail.com>
2022-09-06 18:59:35 -0400

    Added sponsors page with last year's sponsors

--- src/sections/Sponsors/Sponsors.js
+++ src/sections/Sponsors/Sponsors.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Sponsors.css";
 
+import lastyear from "../../assets/Sponsors/lastyear.svg";
+import flowers from "../../assets/Sponsors/flowers.svg";
+
 const Sponsors = () => {
-  return <div className="sponsors"></div>;
+  return (
+    <div id="sponsors">
+      <div className={"sponsors-container"}>
+        <img src={lastyear} alt={"Last Year"}></img>
+      </div>
+      <img id={"footer_flowers"} src={flowers} alt={"Flowers"}></img>
+    </div>
+  );
 };
 
 export default Sponsors;
