0f326fec0e3c2002d05a41e34827e5cac0486337
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2022-09-12 23:02:48 -0400

    finished speakers component

---
+++ src/sections/Speakers/Speakers.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Speakers.css';
+import butterfly4 from "../../assets/About/butterfly-4.svg";
+
+const Speakers = () => {
+	return(
+        <div className="speakers">
+            <h2>Speakers</h2>
+            <p id='speakers-blurb'>HackDuke brings in industry leaders, startup founders, and other impressive humans for talks and workshops. Learn, grow, and get inspired.</p>
+            <div className="speaker-info-container">
+                <div className="speaker-info">
+                    <div className="speaker-name">
+                        <img src="" alt="" className="" />    
+                    </div>
+                    <h4>Speaker name</h4>
+                    <p>description</p>
+                </div>
+
+                <div className="speaker-info">
+                    <div className="speaker-name">
+                        <img src="" alt="" className="" />    
+                    </div>
+                    <h4>Speaker name</h4>
+                    <p>description</p>
+                </div>
+
+                <div className="speaker-info">
+                    <div className="speaker-name">
+                        <img src="" alt="" className="" />    
+                    </div>
+                    <h4>Speaker name</h4>
+                    <p>description</p>
+                </div>
+
+                <div className="speaker-info">
+                    <div className="speaker-name">
+                        <img src="" alt="" className="" />    
+                    </div>
+                    <h4>Speaker name</h4>
+                    <p>description</p>
+                </div>
+            </div>
+        </div>
+        );
+};
+
+export default Speakers;
