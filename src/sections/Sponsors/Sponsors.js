60039597f73e697f75e28c28f1c00f80407c9967
Derek Yuan <derek.yuan@duke.edu>
2022-09-24 17:44:54 -0400

    formatted, and removed colab sponsor

--- src/sections/Sponsors/Sponsors.js
+++ src/sections/Sponsors/Sponsors.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Sponsors.css";
 
-
 import flowers from "../../assets/Sponsors/flowers.svg";
 
-const lineage = { img: "lineage.svg", link: "https://www.lineagelogistics.com/" };
+const lineage = {
+  img: "lineage.svg",
+  link: "https://www.lineagelogistics.com/",
+};
 const js = { img: "js.png", link: "https://www.janestreet.com/" };
-const microsoft = { img: "microsoft.png", link: "https://www.microsoft.com/en-us/" };
-const colab = { img: "colab.png", link: "https://colab.duke.edu/" };
+const microsoft = {
+  img: "microsoft.png",
+  link: "https://www.microsoft.com/en-us/",
+};
 const citadel = { img: "citadel.svg", link: "https://www.citadel.com/" };
 const hrt = { img: "hrt.svg", link: "https://www.hudsonrivertrading.com/" };
 const imc = { img: "imc.svg", link: "https://www.imc.com/us/" };
@@ -15,38 +19,40 @@ const iid = { img: "iid.png", link: "https://bigdata.duke.edu/" };
 const cs = { img: "cs.png", link: "https://cs.duke.edu/" };
 const mems = { img: "mems.svg", link: "https://mems.duke.edu/" };
 
-
 const Sponsors = () => {
- const getSponsorContent=(sponsor)=>{
-  return(
-    <div className={"sponsors-col"}>
-      <a href={sponsor.link} target={"_blank"}>
-        <img className={'sponsors-img'} src={`/sponsors/${sponsor.img}`} alt={sponsor.img}/>
-      </a>
+  const getSponsorContent = (sponsor) => {
+    return (
+      <div className={"sponsors-col"}>
+        <a href={sponsor.link} target={"_blank"}>
+          <img
+            className={"sponsors-img"}
+            src={`/sponsors/${sponsor.img}`}
+            alt={sponsor.img}
+          />
+        </a>
+      </div>
+    );
+  };
+  return (
+    <div className={"width-container"}>
+      <div className={"sponsors-row"}>
+        {getSponsorContent(lineage)}
+        {getSponsorContent(microsoft)}
+        {getSponsorContent(js)}
+      </div>
+      <div className={"sponsors-row"}>
+        {getSponsorContent(hrt)}
+        {getSponsorContent(mems)}
+        {getSponsorContent(citadel)}
+      </div>
+      <div className={"sponsors-row"}>
+        {getSponsorContent(imc)}
+        {getSponsorContent(iid)}
+        {getSponsorContent(cs)}
+      </div>
+      <img id={"footer_flowers"} src={flowers} alt={"Flowers"}></img>
     </div>
   );
-};
-return (
-  <div className={"width-container"}>
-    <div className={"sponsors-row"}>
-    {getSponsorContent(lineage)}
-    {getSponsorContent(microsoft)}
-    {getSponsorContent(js)}
-  </div><div className={"sponsors-row"}>
-    {getSponsorContent(colab)}
-    {getSponsorContent(hrt)}
-    {getSponsorContent(citadel)}
-  </div><div className={"sponsors-row"}>
-    {getSponsorContent(imc)}
-    {getSponsorContent(iid)}
-    {getSponsorContent(cs)}
-  </div><div className={"sponsors-row"}>
-    {getSponsorContent(mems)}
-  </div>
-  <img id={"footer_flowers"} src={flowers} alt={"Flowers"}></img>
-  </div>
-  
-);
 };
 
 export default Sponsors;
