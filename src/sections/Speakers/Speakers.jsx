af6a05ff799af29002ba03659d5db7652c1a950c
kevinkong03 <kevin.kong@duke.edu>
2022-09-27 13:20:47 -0400

    error with display

--- src/sections/Speakers/Speakers.jsx
+++ src/sections/Speakers/Speakers.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Speakers.css';
+import SpeakerSlide from "../SpeakerSlide/SpeakerSlide.js";
 
 const Speakers = () => {
 	return(
         <div id="speakers" className="speakers">
             <h2>Speakers</h2>
             <p id='speakers-blurb'>HackDuke brings in industry leaders, startup founders, and other impressive humans for talks and workshops. Learn, grow, and get inspired.</p>
-            <div className="speaker-info-container">
+            {/* <div className="speaker-info-container">
                 <div className="speaker-info">
                     <div className="speaker-name">
                         <img src="" alt="" className="" />    
@@ -38,7 +39,8 @@ const Speakers = () => {
                     <h4>Speaker name</h4>
                     <p>description</p>
                 </div>
-            </div>
+            </div> */}
+            <SpeakerSlide />
         </div>
         );
 };
