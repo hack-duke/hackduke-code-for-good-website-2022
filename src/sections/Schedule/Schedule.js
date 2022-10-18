f5f84f213822019aaf3dc7ca399e5f4072669d0e
Graham Gumbert <gjgumbert@gmail.com>
2022-10-18 16:38:50 -0400

    updated schedule

--- src/sections/Schedule/Schedule.js
+++ src/sections/Schedule/Schedule.js
@@ -24,16 +24,16 @@ const Schedule = () => {
       event: "Ideation Workshop and Sponsor Fair",
     },
     {
+      time: "1:00-2:00 pm",
+      event: "Product Management Case Study Talk",
+    },
+    {
       time: "2:00-3:00 pm",
       event: "Environment and Inequality Track Talks",
     },
     {
       time: "3:00-4:00 pm",
       event: "Health and Education Track Talks",
-    },
-    {
-      time: "4:00-5:00 pm",
-      event: "Puppy Kindergarten!",
     },
     {
       time: "5:00-6:00 pm",
